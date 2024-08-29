7ae5cddc24e37de73bf5662bf6adfa701a50a6b0
Tarun Chauhan <chauhan271singh@gmail.com>
2024-08-29 12:30:53 +0530

    conflicts resolved

--- src/.d.ts
+++ src/.d.ts
@@ -1,5 +1,6 @@
+
 declare module "*.svg" {
   import * as React from "react";
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default content;
 }
